9e21d6a2a417bcab0262ac0a15b616737e01fd07
Rishit Chakraborty <125457882+recursioncat@users.noreply.github.com>
2025-05-07 00:34:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,53 +1,33 @@
-from flask import Flask, request, jsonify, redirect
-from tensorflow import keras
+from flask import Flask, request, jsonify
 import keras
-from dependencies import *
-import numpy as np
 from PIL import Image
-from io import BytesIO
+import numpy as np
+from dependencies import processImageFromPIL, predictPotato, predictTomato
 
 app = Flask(__name__)
 
 @app.route('/classifyUnknown', methods=['POST'])
 def classify():
-
-    model = keras.models.load_model('Models/Classifier.keras')
+    model = keras.models.load_model('./Models/Classifier.keras')
     possiblePredictions = ['Potato', 'Tomato']
-    
+
     if 'image' not in request.files:
         return jsonify({'error': 'No image part'}), 400
-    
+
     file = request.files['image']
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
 
     image = Image.open(file.stream).convert('RGB')
-    image = processImage(image, 128)  # modify processImage to accept PIL images
-    prediction = possiblePredictions[np.argmax(model.predict(image))]
+    image_array = processImageFromPIL(image, 128)
+    prediction = possiblePredictions[np.argmax(model.predict(image_array))]
 
-    veggie = globals().get('predict'+prediction)
-    result = {'vegType': prediction, 'disease': veggie(file)}
-    return jsonify(result)
+    veggie_func = globals().get('predict' + prediction)
+    if not veggie_func:
+        return jsonify({'error': f"No prediction function for {prediction}"}), 500
+
+    disease = veggie_func(image)
+    return jsonify({'vegType': prediction, 'disease': disease})
 
 
-
-@app.route('/potato')
-def potato():
-    data = request.get_json(force = True)
-    url = data['url']
-    return predictPotato(url)
-    
-@app.route('/tomato')
-def tomato():
-    data = request.get_json(force = True)
-    url = data['url']
-    return predictTomato(url)
-
-@app.route('/')
-def hello():
-    return "Hello from the Classification Server"
-    
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7860)
- 
+app.run(debug=True)
